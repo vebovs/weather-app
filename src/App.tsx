e60ae761b42ec339eccee64fbe2a16b629c2ce94
vebovs <vebovs@hotmail.no>
2021-03-07 21:54:28 +0100

    workflow test

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,9 @@ function App({}: AppProps) {
   }, [count, setCount]);
   // Return the App component.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -25,10 +25,10 @@ function App({}: AppProps) {
         </p>
         <p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             Learn React
           </a>
