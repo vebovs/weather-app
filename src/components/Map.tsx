4c1090cb2f8eab4e593ddeff1f592fc0cd0c9d26
vebovs <vebovs@hotmail.no>
2021-03-11 22:58:09 +0100

    display marker where user clicks

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,29 @@
 import L from 'leaflet';
 import type { LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
-import React from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  useMapEvents,
+  ZoomControl,
+  Marker,
+  Popup,
+} from 'react-leaflet';
+import React, { useState } from 'react';
+
+function LocationMarker() {
+  const [position, setPosition] = useState<LatLngExpression>();
+  const map = useMapEvents({
+    click: (event) => {
+      console.log(event);
+      setPosition(event.latlng);
+    },
+  });
+
+  if (position) return <Marker position={position} />;
+
+  return null;
+}
 
 function Map() {
   // Default coordinates set to Trondheim, Norway
@@ -10,12 +31,18 @@ function Map() {
   const zoom: number = 14;
 
   return (
-    <MapContainer center={position} zoom={zoom} zoomControl={false}>
+    <MapContainer
+      center={position}
+      zoom={zoom}
+      zoomControl={false}
+      doubleClickZoom={false}
+    >
       <TileLayer
         attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
       <ZoomControl position='bottomright' />
+      <LocationMarker />
     </MapContainer>
   );
 }
