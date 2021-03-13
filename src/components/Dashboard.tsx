5863413672835ecf539058fd82260315b1b27fe2
vebovs <vebovs@hotmail.no>
2021-03-13 22:30:10 +0100

    dashboard structure

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
 import { usePosition } from './Position';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Alert,
+  Button,
+  Text,
+  HStack,
+  Container,
+} from '@chakra-ui/react';
+import { Search2Icon } from '@chakra-ui/icons';
 
 function Dashboard() {
   const { position } = usePosition()!;
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  if (position) return <h1>Dashboard!</h1>;
-
-  return null;
+  if (position)
+    return (
+      <Container>
+        <Alert
+          status='success'
+          variant='sublet'
+          flexDirection='column'
+          alignItems='center'
+          justifyContent='center'
+          textAlign='center'
+        >
+          <HStack>
+            <Text>Search for meteorological data</Text>
+            <Button size='md' onClick={onOpen}>
+              <Search2Icon />
+            </Button>
+          </HStack>
+        </Alert>
+        <Drawer isOpen={isOpen} placement='top' onClose={onClose}>
+          <DrawerOverlay>
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerHeader>Dashboard</DrawerHeader>
+              <DrawerBody>
+                <h1>Content goes here!</h1>
+              </DrawerBody>
+            </DrawerContent>
+          </DrawerOverlay>
+        </Drawer>
+      </Container>
+    );
 }
 
 export default Dashboard;
