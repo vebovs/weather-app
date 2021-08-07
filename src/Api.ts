6c13de0c3880e46a1b989f218872e67c5c555940
vebovs <vebovs@hotmail.no>
2021-08-07 22:19:41 +0200

    displays weather data for selected location

--- src/Api.ts
+++ src/Api.ts
@@ -1,5 +1,19 @@
 import axios from 'axios';
+import type { LatLngExpression } from 'leaflet';
 
-const api = axios.create({
-  baseURL: '',
-});
+const api = axios.create();
+
+export const getWeather = (position: LatLngExpression) => {
+  const posString = position.toString();
+  const comPos = posString.indexOf(',');
+  const lat = parseFloat(posString.substring(0, comPos));
+  const lon = parseFloat(posString.substring(comPos + 1, posString.length));
+
+  return api
+    .get(
+      `https://api.met.no/weatherapi/nowcast/2.0/complete?lat=${lat}&lon=${lon}`,
+    )
+    .then((res) => {
+      return res.data;
+    });
+};
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { usePosition } from './Position';
+import Content from './Content';
 import {
   Drawer,
   DrawerBody,
@@ -17,40 +17,38 @@ import {
 import { Search2Icon } from '@chakra-ui/icons';
 
 function Dashboard() {
-  const { position } = usePosition()!;
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  if (position)
-    return (
-      <Container>
-        <Alert
-          status='success'
-          variant='sublet'
-          flexDirection='column'
-          alignItems='center'
-          justifyContent='center'
-          textAlign='center'
-        >
-          <HStack>
-            <Text>Search for meteorological data</Text>
-            <Button size='md' onClick={onOpen}>
-              <Search2Icon />
-            </Button>
-          </HStack>
-        </Alert>
-        <Drawer isOpen={isOpen} placement='top' onClose={onClose}>
-          <DrawerOverlay>
-            <DrawerContent>
-              <DrawerCloseButton />
-              <DrawerHeader>Dashboard</DrawerHeader>
-              <DrawerBody>
-                <h1>Content goes here!</h1>
-              </DrawerBody>
-            </DrawerContent>
-          </DrawerOverlay>
-        </Drawer>
-      </Container>
-    );
+  return (
+    <Container>
+      <Alert
+        status='success'
+        variant='sublet'
+        flexDirection='column'
+        alignItems='center'
+        justifyContent='center'
+        textAlign='center'
+      >
+        <HStack>
+          <Text>Search for meteorological data</Text>
+          <Button size='md' onClick={onOpen}>
+            <Search2Icon />
+          </Button>
+        </HStack>
+      </Alert>
+      <Drawer isOpen={isOpen} placement='top' onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Weather information</DrawerHeader>
+            <DrawerBody>
+              <Content />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </Container>
+  );
 }
 
 export default Dashboard;
