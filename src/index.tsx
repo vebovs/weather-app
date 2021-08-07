5de6c260137db521c40544ecbef70cb518ddc547
vebovs <vebovs@hotmail.no>
2021-08-07 19:25:44 +0200

    fixed error in react devtools import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query-devtools';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
